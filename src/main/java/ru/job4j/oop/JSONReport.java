bf6d139afcf8cb89b27cad78bd71a5c7ec7df6bc
shabad1n <shabadin@icloud.com>
2021-07-08 13:30:59 +0300

    4. Переопределение [# 174890 # 175131]

--- src/main/java/ru/job4j/oop/JSONReport.java
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -4,7 +4,7 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "{" + "\n" + name + ": name," + "\n" + body + ": body" + "\n" + "}";
+        return "{" + System.lineSeparator() + name + ": name," + System.lineSeparator() + body + ": body" + System.lineSeparator() + "}";
     }
 
     public static void main(String[] args) {
