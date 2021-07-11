4549cb8efec51c6a0b5ffb842d8e216e5df542d1
shabad1n <Karabanovo0308>
2021-07-11 10:50:05 +0300

    6. Дата. Отображение даты. [#310017 #175716]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,8 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-         System.out.println(item.getCreated());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = item.getCreated().format(formatter);
+         System.out.println(currentDateTimeFormat);
     }
 }
