cf901a6bf169008e6bad19f51aa626fe7a9deac9
shabad1n <shabadin@icloud.com>
2021-07-13 14:06:39 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,37 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemFindAll = new Item[items.length];
+        itemFindAll = Arrays.copyOf(items, size);
+        return itemFindAll;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] arrayName = new Item[items.length];
+        int temp = 0;
+        for(int i = 0; i < size; i++) {
+            Item item = items[i];
+            if(item.getName() == key) {
+                arrayName[i] = item;
+                temp++;
+            }
+        }
+        arrayName = Arrays.copyOf(arrayName, temp);
+        return arrayName;
+    }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(new Item("First"));
+        tracker.add(new Item("Second"));
+        tracker.add(new Item("First"));
+        tracker.findByName(first.getName());
     }
 }
