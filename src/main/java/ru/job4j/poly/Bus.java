fb258873d122cf852dfb27fb39230d6f3504295e
shabad1n <shabadin@icloud.com>
2021-07-16 10:12:13 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,24 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+
+    @Override
+    public void drive() {
+        int maxSpeed = 130;
+    }
+
+    @Override
+    public void passengers(int passenger) {
+        int maxCountPassan = 60;
+    }
+
+    @Override
+    public int fill(int count) {
+        int sum = 0;
+        if(count > 0) {
+            sum = count * 45;
+        }
+        return sum;
+    }
+}
