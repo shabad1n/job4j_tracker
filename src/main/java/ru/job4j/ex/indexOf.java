d0afaa40ead0399843c88ec716061be057d88713
shabad1n <shabadin@icloud.com>
2021-07-22 09:49:58 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/indexOf.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+    public class indexOf {
+
+        public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+            int rsl = -1;
+            for (int i = 0; i < value.length; i++) {
+                if (value[i].equals(key)) {
+                    System.out.println(i);
+                    rsl = i;
+                } else {
+                    throw new ElementNotFoundException("Совпадений не обнаружено");
+                }
+            }
+            return rsl;
+        }
+
+    public static void main(String[] args) {
+        String[] value = new String[]{"hello", "world", "!"};
+        try {
+            indexOf(value, "Hello");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
