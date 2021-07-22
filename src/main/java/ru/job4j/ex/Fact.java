525dd7255287dfbdb3d90f93bb7bde8ad131f0b7
shabad1n <shabadin@icloud.com>
2021-07-22 08:20:56 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #180132]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -5,14 +5,13 @@ public class Fact {
     public static int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Метод вызывается с некоректным аргументом");
-        } else {
+        }
             int rsl = 1;
             for (int index = 1; index <= n; index++) {
                 rsl *= index;
             }
             return rsl;
         }
-    }
     public static void main(String[] args) {
         new Fact().calc(-1);
     }
