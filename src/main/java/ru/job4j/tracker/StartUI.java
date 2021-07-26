cd0d14cb120e25c9990a7091989f66608dc6948f
shabad1n <shabadin@icloud.com>
2021-07-26 10:03:22 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,6 +12,10 @@ public class StartUI {
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
+                continue;
+            }
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,24 +1,13 @@
 package ru.job4j.tracker;
 
-import java.util.Scanner;
-
-public class ConsoleInput implements Input {
-
-    private Scanner scanner = new Scanner(System.in);
-
-    @Override
-    public String askStr(String question) {
-        System.out.print(question);
-        return scanner.nextLine();
-    }
-
+public class ValidateInput extends ConsoleInput {
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = Integer.parseInt(askStr(question));
+                value = super.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again.");
