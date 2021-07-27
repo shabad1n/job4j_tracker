a4f733e0d6822be87a9b131d3eccb494d1a9fd9d
shabad1n <shabadin@icloud.com>
2021-07-27 11:26:14 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/main/java/ru/job4j/tracker/Test.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class Test {
+    public static void main(String[] args) {
+        int com = Integer.parseInt("-10");
+        System.out.println(com);
+    }
+}
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -184,12 +184,23 @@ public class StartUITest {
     public void ValidatInputSecond() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"One", "-1", "1", "2"}
+                new String[]{"One", "-1", "0"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        int[] results = new int[2];
-        results[0] = input.askInt("Enter menu:");
-        results[1] = input.askInt("Enter menu:");
-        assertArrayEquals(results, new int[]{1, 2});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitProgramAction(out)
+        };
+        new StartUI(out).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "Please enter validate data again." + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu:" + ln
+                        + "0. Exit Program" + ln
+                )
+        );
     }
 }
