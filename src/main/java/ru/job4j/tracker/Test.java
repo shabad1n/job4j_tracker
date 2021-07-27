c3e9a43c547211b55a3016f3fdc2b583caad2afe
shabad1n <shabadin@icloud.com>
2021-07-27 12:43:18 +0300

    3. Замена if-else-throw на if-throw [#49895]

--- src/main/java/ru/job4j/tracker/Test.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.tracker;
-
-public class Test {
-    public static void main(String[] args) {
-        int com = Integer.parseInt("-10");
-        System.out.println(com);
-    }
-}
