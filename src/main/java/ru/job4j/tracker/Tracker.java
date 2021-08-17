2813ef9bf78e969ffb120af829637dba180952bd
shabad1n <shabadin@icloud.com>
2021-08-17 15:00:02 +0300

    0. Set, HashSet, Iterator. [#10095]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -27,7 +27,6 @@ public class Tracker {
 
     public List<Item> findAll() {
         List copy = List.copyOf(items);
-        List<Item> copy1 = new ArrayList<>(items);
         return copy;
     }
 
