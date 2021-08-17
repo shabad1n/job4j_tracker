0413f43f35e0dfb293b00d987425faf3e4eb7897
shabad1n <shabadin@icloud.com>
2021-08-17 15:55:16 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for(Task task : list) {
+           numbers.add(task.getNumber());
+        }
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = Arrays.asList(new Task("1", "first task"), new Task("2", "second task"),
+                new Task("1", "first task"));
+        extractNumber(tasks);
+    }
+}
