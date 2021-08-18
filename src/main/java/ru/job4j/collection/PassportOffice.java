03e0d658675db42ae5c3880dca8996444d78a498
shabad1n <shabadin@icloud.com>
2021-08-18 16:04:53 +0300

    1. Паспорт и жители [#212656]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+        }
+        return rsl;
+    }
+
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+
+    public static void main(String[] args) {
+        Citizen citizen1 = new Citizen("2f44a", "Kirill Gavrilov");
+        Citizen citizen2 = new Citizen("2f45A", "Kirill Gavrilov");
+        PassportOffice passportOffice = new PassportOffice();
+        passportOffice.add(citizen1);
+        passportOffice.add(citizen2);
+        passportOffice.get("2f44a");
+    }
+}
