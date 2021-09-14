4f57958c944a7b966a42fae38164bda9e3730e54
shabad1n <shabadin@icloud.com>
2021-09-14 14:30:38 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -11,7 +11,7 @@ public class AttachmentSort {
                 new Attachment("image 2", 34),
                 new Attachment("image 3", 13)
         );
-        Comparator<Attachment> comparator =  new Comparator() {
+        Comparator comparator =  new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
                 Attachment left = (Attachment) o1;
