5bfd32862cad8ea4c57f82219663989791176804
shabad1n <shabadin@icloud.com>
2021-12-01 12:35:30 +0300

    0.1. Отладка лямбда в IDEA [#259165 #234378]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -2,6 +2,7 @@ package ru.job4j.stream;
 
 import java.util.List;
 public class StreamUsage {
+
     public static class Task {
         private final String name;
         private final long spent;
