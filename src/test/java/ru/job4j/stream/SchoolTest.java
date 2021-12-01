897fcd73aaf6047c60c3888e5e540fcf7623bf47
shabad1n <shabadin@icloud.com>
2021-12-01 13:31:07 +0300

    1. Фильтрация учеников [#110058 #234713]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -10,6 +10,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SchoolTest {
+
     @Test
     public void whenCollectClassA() {
         List<Student> students = List.of(
