a7aa5f1c7a19a5af244c000f36c9f01468b0d7d4
shabad1n <shabadin@icloud.com>
2021-12-14 16:47:51 +0300

    3. Optional в банковских переводах. [#214711]

---
+++ src/main/java/ru/job4j/stream/attestation/College.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream.attestation;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class College {
+    private final Map<Student, Set<Subject>> students;
+
+    public College(Map<Student, Set<Subject>> students) {
+        this.students = students;
+    }
+
+    public Optional<Student> findByAccount(String account) {
+       return students.keySet().stream()
+                .filter(student -> student.getAccount().equals(account))
+                .findFirst();
+    }
+
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Student> s = findByAccount(account);
+        Optional<Subject> rsl = Optional.empty();
+        if (s.isPresent()) {
+            return students.get(s).stream().filter(subject -> subject.getName().equals(name))
+                    .findFirst();
+        }
+        return rsl;
+    }
+}
