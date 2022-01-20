a3242469c41c7008708195aa9e8caff93a8eebaf
shabad1n <shabadin@icloud.com>
2022-01-20 10:59:09 +0300

    2. Отсортировать департаменты [#34352 #252325]

--- src/main/java/ru/job4j/collection/sorted/DepDescComp.java
+++ src/main/java/ru/job4j/collection/sorted/DepDescComp.java
@@ -8,9 +8,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] str1 = o1.split("/");
         String[] str2 = o2.split("/");
-        if (str2[0].compareTo(str1[0]) == 0) {
-            return o1.compareTo(o2);
-        }
-        return o2.compareTo(o1);
+        int rsl = str2[0].compareTo(str1[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
